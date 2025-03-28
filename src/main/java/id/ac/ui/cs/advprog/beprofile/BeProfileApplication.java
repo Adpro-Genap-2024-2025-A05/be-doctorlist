b96800b44a98105e9b9a86defb612c6ed0e4e7fb
Bastian2312 <bastian.adiputras@gmail.com>
2025-03-28 17:59:30 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/beprofile/BeProfileApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeProfileApplication.class, args);
+    }
+
+}
