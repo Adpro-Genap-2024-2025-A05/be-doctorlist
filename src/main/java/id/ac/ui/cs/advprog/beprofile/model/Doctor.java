0d8e58602699fb3fbcc2ebda1ab6835e064303ac
FigoFavian <figo21fr@gmail.com>
2025-04-08 17:01:56 +0700

    [REFACTORING] Updating Doctor model

--- src/main/java/id/ac/ui/cs/advprog/beprofile/model/Doctor.java
+++ src/main/java/id/ac/ui/cs/advprog/beprofile/model/Doctor.java
@@ -1,5 +1,10 @@
 package id.ac.ui.cs.advprog.beprofile.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Doctor {
     private String id;
     private String name;
@@ -8,54 +13,4 @@ public class Doctor {
     private String email;
     private String phoneNumber;
     private double rating;
-
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPracticeAddress() {
-        return practiceAddress;
-    }
-    public void setPracticeAddress(String practiceAddress) {
-        this.practiceAddress = practiceAddress;
-    }
-
-    public String getWorkSchedule() {
-        return workSchedule;
-    }
-    public void setWorkSchedule(String workSchedule) {
-        this.workSchedule = workSchedule;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
 }
