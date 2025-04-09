0c948527b501794e71caf1829ab2313c544b2b62
syarna <syarnasavitri66@gmail.com>
2025-04-10 00:26:24 +0700

    create user class

---
+++ model/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private String id;
+    private String fullname;
+    private String email;
+    private String gender;
+    private String username;
+    private String password;
+    private Role role;
+}
