337b351a83a4d796e0b6f2f636a972221650ff90
FigoFavian <figo21fr@gmail.com>
2025-04-09 11:30:29 +0700

    [REFACTORING] refactoring test location

--- src/test/java/id/ac/ui/cs/advprog/beprofile/model/DoctorTest.java
+++ src/test/java/id/ac/ui/cs/advprog/beprofile/model/DoctorTest.java
@@ -1,6 +1,5 @@
-package id.ac.ui.cs.advprog.beprofile;
+package id.ac.ui.cs.advprog.beprofile.model;
 
-import id.ac.ui.cs.advprog.beprofile.model.Doctor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
