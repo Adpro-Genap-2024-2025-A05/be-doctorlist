f36a3c00194a677205fb934498b3a6f94892632f
FigoFavian <figo21fr@gmail.com>
2025-04-11 20:59:02 +0700

    fixing test

--- src/test/java/id/ac/ui/cs/advprog/beprofile/BeProfileApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/beprofile/BeProfileApplicationTests.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.advprog.beprofile;
 
+import id.ac.ui.cs.advprog.beprofile.service.ProfileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,9 +14,11 @@ class BeProfileApplicationTests {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @MockBean
+    private ProfileService profileService;
+
     @Test
     void contextLoads() {
-        // Verify that the Spring application context loads successfully
         assertThat(applicationContext).isNotNull();
     }
 }
