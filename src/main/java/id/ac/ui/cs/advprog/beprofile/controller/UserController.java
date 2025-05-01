790735022a85035e4acb2130ba953ce8673c0f58
syarna <syarnasavitri66@gmail.com>
2025-05-01 21:25:05 +0700

    [feat] Add ConsultationHistoryRepository for database access

---
+++ src/main/java/id/ac/ui/cs/advprog/beprofile/controller/UserController.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.beprofile.controller;
+
+import id.ac.ui.cs.advprog.beprofile.model.User;
+import id.ac.ui.cs.advprog.beprofile.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserProfile(@PathVariable String id) {
+        User user = userService.getUserProfile(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUserProfile(@PathVariable String id, @RequestBody User updatedUser) {
+        User user = userService.updateUserProfile(id, updatedUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserAccount(@PathVariable String id) {
+        userService.deleteUserAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+}
