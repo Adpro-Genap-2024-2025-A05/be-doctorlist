df4c33457dd709b39958a3b050e7422e933e7daf
syarna <syarnasavitri66@gmail.com>
2025-05-12 16:10:15 +0700

    [GREEN] Add search strategies for flexible doctor search

--- src/main/java/id/ac/ui/cs/advprog/beprofile/service/strategy/SearchStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/beprofile/service/strategy/SearchStrategy.java
@@ -1,12 +1,9 @@
 package id.ac.ui.cs.advprog.beprofile.service.strategy;
 
-
 import id.ac.ui.cs.advprog.beprofile.model.Doctor;
 
 import java.util.List;
 
 public interface SearchStrategy {
-
     List<Doctor> search(List<Doctor> doctors, String criteria);
 }
-
