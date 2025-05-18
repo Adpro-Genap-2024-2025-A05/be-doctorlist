93cf60f2eb16effbaa71f621c8572c2b1951cd0b
FigoFavian <figo21fr@gmail.com>
2025-05-18 15:50:23 +0700

    [GREEN] implement DoctorRepository with JPA, remove InMemory version

--- src/main/java/id/ac/ui/cs/advprog/beprofile/repository/DoctorRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/beprofile/repository/DoctorRepository.java
@@ -2,7 +2,9 @@ package id.ac.ui.cs.advprog.beprofile.repository;
 
 import id.ac.ui.cs.advprog.beprofile.model.Doctor;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface DoctorRepository extends WriteRepository<Doctor>, ReadRepository<Doctor> {
+public interface DoctorRepository extends JpaRepository<Doctor, String> {
+
 }
